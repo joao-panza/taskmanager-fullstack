ac897b00c9ad8aefd0903b3983555982c5d6cc7b
João Miguel <joaomiguel.apr@gmail.com>
2024-09-27 03:23:31 -0300

    feat: implementing field validations on request

--- backend/src/middlewares/tasksMiddleware.js
+++ backend/src/middlewares/tasksMiddleware.js
@@ -1,10 +1,10 @@
-const validateBody = (req, res, next) => {
+const validateFieldTitle = (req, res, next) => {
     const { body } = req;
-    // Valida se o título da tarefa está preenchido.
+    // Valida se o título da tarefa foi enviado na requisição
     if(body.title === undefined){
         return res.status(400).json({message:'Campo título é obrigatório.'});
     }
-    // 
+    // Valida se o título está vazio
     if(body.title === ''){
         return res.status(400).json({message:'Título não pode ser vazio.'});
     }
@@ -12,6 +12,20 @@ const validateBody = (req, res, next) => {
     next();
 };
 
+const validateFieldStatus = (req, res, next) => {
+    const { body } = req;
+    if(body.status === undefined){
+        return res.status(400).json({message:'Status não informado.'});
+    }
+    // Valida se o status está vazio
+    if(body.status === ''){
+        return res.status(400).json({message:'Campo status não pode ser vazio.'});
+    }
+
+    next();
+};
+
 module.exports = {
-    validateBody,
+    validateFieldTitle,
+    validateFieldStatus,
 };
