50b0aaadc9b1df68abe1ad3ed9bb6c4254147088
João Miguel <joaomiguel.apr@gmail.com>
2024-10-02 01:24:17 -0300

    feat: created addTask and deleteTask functions

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,20 +1,89 @@
+const ul = document.querySelector('ul');
+const addForm = document.querySelector('.add-form');
+const inputTask = document.querySelector('.input-task');
+
 const fetchTasks = async () =>{
    const response = await fetch('http://localhost:3333/tasks');
    const tasks = await response.json();
    console.log(tasks);
    return tasks;
-};
-
-const task = {
-    id: 3,
-    title: "salve caraio",
-    status: "disponível",
-    created_at: "24/09/2024"
-};
-
-const createTask = (task) =>{
-    const { title } = task;
-    const li = document.createElement('li');
 }
 
-createTask(task);
+// Inclui a tarefa no banco e exibe a lista de tarefas após inclusão
+const addTask = async(event) =>{
+    event.preventDefault();
+    
+    const task = { title: inputTask.value };
+
+    ul.innerHTML = '';
+
+    // Faz uma chamada para o back inserir (post) o registro no banco
+    await fetch('http://localhost:3333/tasks/', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(task),
+    });
+    loadTasks();
+    // Limpa o campo de input após inclusão de tarefa
+    inputTask.value = '';
+}
+
+// Remove a tarefa do banco e recarrega a página
+const deleteTask = async (id) =>{
+    await fetch(`http://localhost:3333/tasks/${id}`, {
+        method: 'delete',
+    });
+    
+    loadTasks();
+}
+
+// Responsável por criar o elemento html 
+const createElement = (tag, innerText = '', innerHTML = '') => {
+    const element = document.createElement(tag);
+
+    if(innerText){
+        element.innerText = innerText;
+    }
+
+    if(innerHTML){
+        element.innerHTML = innerHTML;
+    }
+    return element;
+}
+
+// Insere a tag dentro do html 
+const createTask = (task) =>{
+    const { id, title, status, created_at } = task;
+    const li = createElement('li');
+    const liTitle = createElement('div', title);
+    const divButtons = createElement('div');
+
+    const completeButton = createElement('button', '', '<span class="material-symbols-outlined">check_small</span>');
+    const deleteButton = createElement('button', '', '<span class="material-symbols-outlined">close</span>');
+    
+    completeButton.classList.add('btn-complete');
+    deleteButton.classList.add('btn-delete');
+
+    deleteButton.addEventListener('click', () => deleteTask(id));
+
+    divButtons.appendChild(completeButton);
+    divButtons.appendChild(deleteButton);
+
+    li.appendChild(liTitle);
+    li.appendChild(divButtons);
+
+    return li;
+}
+
+// Carrega as tarefas na tela com base no retorno da função fetchTasks
+const loadTasks = async () => {
+    const tasks = await fetchTasks ();
+    tasks.forEach((task) => {
+        const li = createTask(task);
+        ul.appendChild(li);
+    });
+}
+
+addForm.addEventListener('submit', addTask);
+
+loadTasks();
