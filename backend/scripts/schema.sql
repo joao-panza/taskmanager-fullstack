2c85ddb6261d91d062b648ef97a49ddc99655c0a
João Miguel <joaomiguel.apr@gmail.com>
2025-05-15 23:24:54 -0300

    feat: added docker-compose

---
+++ backend/scripts/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `TASKS` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `created_at` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
